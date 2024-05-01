f02ea5d785d4d6931aacf94036ccf75318dcf44e
fai-sas <craftedbyfaisal@gmail.com>
2024-05-01 10:10:43 +0600

    basic data types

---
+++ src/basic-data-type.ts
@@ -0,0 +1,37 @@
+// string
+
+let firstName: string = 'John'
+
+firstName = 'Jack'
+
+// number
+
+const id: number = 1234
+
+// boolean
+
+const isAdmin: boolean = false
+
+// undefined
+
+let currentUser: undefined = undefined
+
+// null
+
+let searchTerm: null = null
+
+// array
+
+const teams: string[] = ['Chelsea', 'Real Madrid', 'Bayern', 'Arsenal']
+
+let players: string[] = ['Silva', 'Bale', 'Kane', 'Henry']
+
+players.push('James')
+
+// tuple --> returns an array --> order is important --> set the type of values
+
+let numberOfTitle: [number, number] = [2, 5]
+
+let title: [string, number, boolean] = ['UCL', 2, true]
+
+title[1] = 2
